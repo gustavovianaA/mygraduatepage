442442d0e95db0b2d1889417012ca500f52190aa
Gustavo <gustavow3dev@gmail.com>
2021-03-07 12:15:52 -0300

    client structure

---
+++ client/vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '57f7e50de860b288de9816b4c9a74ed92e4b88e5',
+    'name' => 'gva/mygraduatepage.php',
+  ),
+  'versions' => 
+  array (
+    'gva/mygraduatepage.php' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '57f7e50de860b288de9816b4c9a74ed92e4b88e5',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v4.6.0',
+      'version' => '4.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6ffb0b48e455430f8a5359ed689ad64c1143fac2',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v4.6.0',
+      ),
+    ),
+  ),
+);
